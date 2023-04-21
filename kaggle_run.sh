ed663bead4b118ab485a98155c1fc5078b0af791
KGMking <126856248+yald3915@users.noreply.github.com>
2023-04-21 14:48:16 +0800

    Delete kaggle_run.sh

--- kaggle_run.sh
+++
@@ -1,8 +0,0 @@
-python tools/train.py -n yolox-l -c ./pretrain_models/yolox_l.pth -b 64 -f ./starfish_yolox_l.py  -d 1
-
-# -n 训练模型名称，可以修改
-# -c 预模型模型路径
-# -b batchsizem，每一批次大小
-# -f 配置文件，自己在当前目录仿照starfish_yolox_l.py 修改
-# -d 使用几个GPU
-# -o 无实际意义 
